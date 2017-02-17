63452fdf309f733d3a16faeb114a41ea4eb18c09
Benaiah Mischenko <benaiah@mischenko.com>
2017-02-17 02:18:36 -0800

    "play" command and colors

--- index.js
+++ index.js
@@ -2,14 +2,40 @@ const colors = require('colors')
 const feedparser = require('feedparser-promised')
 const fs = require('fs')
 const parseArgs = require('minimist')
+const Player = require('player')
 const ProgressBar = require('progress')
 const request = require('request')
 const requestProgress = require('request-progress')
 
-const feedUrl = "http://musicforprogramming.net/rss.php"
-const tracksLocation = "/home/benaiah/Music/mfp/"
+const logErr = err => console.error(colors.red(err))
 
 const args = parseArgs(process.argv.slice(2))
+
+const feedUrl = "https://musicforprogramming.net/rss.php"
+const tracksPath = (() => {
+  if (args.tracksPath) {
+    return args.tracksPath
+  }
+  else if (process.env.MFP_DOWNLOAD_PATH) {
+    return process.env.MFP_DOWNLOAD_PATH
+  } else if (process.env.HOME) {
+    return process.env.HOME + "/.mfp/"
+  } else {
+    return ""
+  }
+})()
+
+if (tracksPath === "") {
+  logErr("Neither HOME nor MFP_TRACKS_PATH is set, cannot guess a"
+         + " path to download tracks to.")
+}
+
+// ensure the download folder exists
+if (!fs.existsSync(tracksPath)) {
+  fs.mkdirSync(tracksPath, 0744)
+}
+
+
 
 //
 // retrieving info
@@ -33,6 +59,10 @@ const getTracksReversed = () =>
 
 const getFilename = url => url.split("/").pop();
 
+const getTrackPath = track => tracksPath + getFilename(track.url)
+
+const isTrackDownloaded = track => fs.existsSync(getTrackPath(track))
+
 
 
 //
@@ -46,8 +76,13 @@ const displayTitle = title => {
   return "[" + index + "] " + split[1];
 }
 
-const displayItem = item =>
-   displayTitle(item.title)
+const displayTrack = track => {
+  if (isTrackDownloaded(track)) {
+    return displayTitle(track.title).green
+  } else {
+    return displayTitle(track.title).red
+  }
+}
 
 
 
@@ -55,44 +90,83 @@ const displayItem = item =>
 // commands
 //
 
+const interactivelyDownload = track => {
+  const path = tracksPath + getFilename(track.url)
+  const bar = new ProgressBar(':bar', { total: 100 })
+
+  if (isTrackDownloaded(track)) {
+    console.log(`Removing old file at ${path}`)
+    fs.unlinkSync(getTrackPath(track))
+  }
+
+  console.log(`Downloading track to ${path}`)
+  return new Promise((resolve, reject) => {
+    requestProgress(request.get(track.url))
+      .on('progress', state => {
+        bar.update(state.percent)
+      })
+      .pipe(fs.createWriteStream(path))
+      .on('close', () => {
+        console.log("")
+        resolve(track)
+      })
+  })
+}
+
 const commands = {
-  help: () => console.log(`
+  help: () => console.log(`\
 ${"m".green}${"f".blue}${"p".red}: a scraper for ${"http://musicforprogramming.net".yellow}
 
-Commands         Args   Description
-list, l          -      (default) list all episodes
-h, help          -      show this help text
-dl, download     NUM    download episode NUM
-path, p          NUM    get path of episode NUM ("mpg123 $(mfp p 20)" would play ep. 20)
+Flags
+--tracks-path     set a path to download tracks to (will be created if
+                  it doesn't exist). can also be set with MFP_TRACKS_PATH.
+                  Defaults to "$HOME/.mfp/" if not set.
+
+Commands          Args   Description
+list, l           -      (default) list all episodes
+  --reverse, -r   -      list reversed
+
+h, help           -      show this help text
+dl, download      NUM    download episode NUM
+path              NUM    get path of episode NUM ("mpg123 $(mfp p 20)" would play ep. 20)
+play, p           NUM    play episode NUM (plays downloaded file if present, streams otherwise)\
+  --save, s       NUM    play episode NUM (always downloads if file not present)
 `),
 
-  list: () =>
-    getTracksReversed()
-    .then(items => items.map(displayItem))
+  list: () => ((args.r || args.reverse) ? getTracks() : getTracksReversed())
+    .then(items => items.map(displayTrack))
     .then(lines => lines.forEach(line => console.log(line)))
-    .catch(err => console.err(err)),
+    .catch(err => console.error(err)),
 
-  download: index =>
-    getTracks()
+  download: index => getTracks()
     .then(tracks => tracks[tracks.length - index])
-    .then(track => {
-      const path = tracksLocation + getFilename(track.url)
-      const bar = new ProgressBar(':bar', { total: 100 })
+    .then(interactivelyDownload)
+    .catch(err => console.error(err)),
 
-      console.log(`Downloading `)
-
-      requestProgress(request.get(track.url))
-        .on('progress', state => {
-          bar.update(state.percent)
-        })
-        .pipe(fs.createWriteStream(path))
-    })
-    .catch(err => console.log(err)),
-
-  path: index =>
-    getTracks()
+  path: index => getTracks()
     .then(tracks => tracks[tracks.length - index])
-    .then(track => console.log(tracksLocation + getFilename(track.url)))
+    .then(track => console.log(tracksPath + getFilename(track.url)))
+    .catch(err => console.error(err)),
+
+  play: index => {
+    getTracks()
+      .then(tracks => tracks[tracks.length - index])
+      .then(track => (!isTrackDownloaded(track) && (args.save || args.s))
+         ? interactivelyDownload(track)
+         : track)
+      .then(track => {
+        if (isTrackDownloaded(track)) {
+          const player = new Player(getTrackPath(track))
+          console.log(`Playing ${getTrackPath(track)}...`)
+          player.play()
+        } else {
+          const player = new Player(track.url)
+          console.log(`Streaming ${track.url}`)
+          player.play()
+        }
+      })
+      .catch(err => console.error(err))
+  }
 }
 
 // subcommands
@@ -116,9 +190,14 @@ case "dl":
   break;
 
 case "path":
-case "p":
   index = args._[1]
   commands.path(index)
+  break;
+
+case "play":
+case "p":
+  index = args._[1] ? args._[1] : (args.s || args.save)
+  commands.play(index)
   break;
 
 default:
